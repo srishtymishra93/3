1bd8ea80f118dae11e53d1a7aab54b4adfc9d16a
srishtymishra93 <122490440+srishtymishra93@users.noreply.github.com>
2023-02-17 00:36:30 +0530

    Add files via upload

---
+++ 14lab2.py
@@ -0,0 +1,24 @@
+#wap to find ASKAI value
+x=str(input("enter any character="))
+print(x)
+#wap to swap the value of two variable without using third variable
+x=int(input("enter the first number:"))
+y=int(input("enter the second number:"))
+print("x=",x,"y=",y,)
+print("after swaping")
+x=x+y
+y=x-y
+x=x-y
+print("x=",x,"y=",y,)
+
+
+#wap to perform the type casting in python
+x=int(input("enter first number"))
+y=int(input("enter second number"))
+z=float(x+y)
+print(z)
+
+x=3
+y=8.9
+z=x+y
+print(z)
